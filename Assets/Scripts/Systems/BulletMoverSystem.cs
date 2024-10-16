a87a6dbae29c76841de28840463a66c1fc95b481
GhiCV <caghi2001@gmail.com>
2024-10-16 16:35:54 +0700

    ghicv: BulletMoverJob, FindTargetJob

--- Assets/Scripts/Systems/BulletMoverSystem.cs
+++ Assets/Scripts/Systems/BulletMoverSystem.cs
@@ -1,52 +1,139 @@
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
-
 partial struct BulletMoverSystem : ISystem
 {
+    private ComponentLookup<Health> _healthComponentLookup;
+    private ComponentLookup<ShootVictim> _shootVictimComponentLookup;
+    private ComponentLookup<LocalTransform> _localTransformComponentLookup;
+
+    private EntityCommandBuffer ecb;
+
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        _healthComponentLookup = state.GetComponentLookup<Health>();
+        _shootVictimComponentLookup = state.GetComponentLookup<ShootVictim>(true);
+        _localTransformComponentLookup = state.GetComponentLookup<LocalTransform>();
+    }
+
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        EntityCommandBuffer ecb = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged);
+        ecb = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged);
 
-        foreach ((RefRW<LocalTransform> localTransform, RefRO<Bullet> bullet, RefRO<Target> target, Entity entity) 
-            in SystemAPI.Query<RefRW<LocalTransform>, RefRO<Bullet>, RefRO<Target>>().WithEntityAccess())
+        #region With Job
+
+        _healthComponentLookup.Update(ref state);
+        _shootVictimComponentLookup.Update(ref state);
+        _localTransformComponentLookup.Update(ref state);
+
+        BulletMoverJob bulletMoveJob = new BulletMoverJob
         {
-            if (target.ValueRO.targetEntity == Entity.Null)
-            {
-                ecb.DestroyEntity(entity);
-                continue;
-            }
+            ecb = ecb,
+            deltaTime = SystemAPI.Time.DeltaTime,
+            healthComponentLookup = _healthComponentLookup,
+            localTransformComponentLookup = _localTransformComponentLookup,
+            shootVictimComponentLookup = _shootVictimComponentLookup
+        };
 
-            LocalTransform targetLocalTransform = SystemAPI.GetComponent<LocalTransform>(target.ValueRO.targetEntity);
-            ShootVictim targetShootVictiom = SystemAPI.GetComponent<ShootVictim>(target.ValueRO.targetEntity);
-            float3 targetPosition = targetLocalTransform.TransformPoint(targetShootVictiom.hitLocalPosition);
+        bulletMoveJob.ScheduleParallel();
+        #endregion
 
-            float distanceBefor = math.distancesq(localTransform.ValueRO.Position, targetPosition);
+        #region Without Job
 
-            float3 moveDirection = targetPosition - localTransform.ValueRO.Position;
-            moveDirection = math.normalize(moveDirection);
-            localTransform.ValueRW.Position += moveDirection * bullet.ValueRO.speed * SystemAPI.Time.DeltaTime;
+        //foreach ((RefRW<LocalTransform> localTransform, RefRO<Bullet> bullet, RefRO<Target> target, Entity entity) 
+        //    in SystemAPI.Query<RefRW<LocalTransform>, RefRO<Bullet>, RefRO<Target>>().WithEntityAccess())
+        //{
+        //if (target.ValueRO.targetEntity == Entity.Null)
+        //{
+        //    ecb.DestroyEntity(entity);
+        //    continue;
+        //}
 
-            float distanceAfter = math.distancesq(localTransform.ValueRO.Position, targetPosition);
+        //LocalTransform targetLocalTransform = SystemAPI.GetComponent<LocalTransform>(target.ValueRO.targetEntity);
+        //ShootVictim targetShootVictiom = SystemAPI.GetComponent<ShootVictim>(target.ValueRO.targetEntity);
+        //float3 targetPosition = targetLocalTransform.TransformPoint(targetShootVictiom.hitLocalPosition);
 
-            if(distanceAfter > distanceBefor)
-            {
-                // Overshot
-                localTransform.ValueRW.Position = targetPosition;
-            }
+        //float distanceBefor = math.distancesq(localTransform.ValueRO.Position, targetPosition);
 
-            float destroyDistanceSq = 0.2f;
+        //float3 moveDirection = targetPosition - localTransform.ValueRO.Position;
+        //moveDirection = math.normalize(moveDirection);
+        //localTransform.ValueRW.Position += moveDirection * bullet.ValueRO.speed * SystemAPI.Time.DeltaTime;
 
-            if (math.distancesq(localTransform.ValueRO.Position, targetPosition) < destroyDistanceSq)
-            {
-                RefRW<Health> targethealth = SystemAPI.GetComponentRW<Health>(target.ValueRO.targetEntity);
-                targethealth.ValueRW.healthAmount -= bullet.ValueRO.damageAmount;
-                targethealth.ValueRW.onHealthChanged = true;
+        //float distanceAfter = math.distancesq(localTransform.ValueRO.Position, targetPosition);
 
-                ecb.DestroyEntity(entity);
-            }
+        //if(distanceAfter > distanceBefor)
+        //{
+        //    // Overshot
+        //    localTransform.ValueRW.Position = targetPosition;
+        //}
+
+        //float destroyDistanceSq = 0.2f;
+
+        //if (math.distancesq(localTransform.ValueRO.Position, targetPosition) < destroyDistanceSq)
+        //{
+        //    RefRW<Health> targethealth = SystemAPI.GetComponentRW<Health>(target.ValueRO.targetEntity);
+        //    targethealth.ValueRW.healthAmount -= bullet.ValueRO.damageAmount;
+        //    targethealth.ValueRW.onHealthChanged = true;
+
+        //    ecb.DestroyEntity(entity);
+        //}
+        //}
+
+        #endregion
+    }
+}
+
+[BurstCompile]
+public partial struct BulletMoverJob : IJobEntity
+{
+    public float deltaTime;
+
+    [NativeDisableParallelForRestriction] public EntityCommandBuffer ecb;
+    [ReadOnly] public ComponentLookup<ShootVictim> shootVictimComponentLookup;
+    [NativeDisableParallelForRestriction] public ComponentLookup<Health> healthComponentLookup;
+    [NativeDisableParallelForRestriction] public ComponentLookup<LocalTransform> localTransformComponentLookup;
+
+    public void Execute(in Bullet bullet, in Target target, Entity entity)
+    {
+        if (target.targetEntity == Entity.Null)
+        {
+            ecb.DestroyEntity(entity);
+            return;
+        }
+
+        RefRW<LocalTransform> localTransform = localTransformComponentLookup.GetRefRW(entity);
+        LocalTransform targetLocalTransform = localTransformComponentLookup[target.targetEntity];
+        ShootVictim targetShootVictiom = shootVictimComponentLookup[target.targetEntity];
+
+        float3 targetPosition = targetLocalTransform.TransformPoint(targetShootVictiom.hitLocalPosition);
+
+        float distanceBefor = math.distancesq(localTransform.ValueRO.Position, targetPosition);
+
+        float3 moveDirection = targetPosition - localTransform.ValueRO.Position;
+        moveDirection = math.normalize(moveDirection);
+        localTransform.ValueRW.Position += moveDirection * bullet.speed * deltaTime;
+
+        float distanceAfter = math.distancesq(localTransform.ValueRO.Position, targetPosition);
+
+        if (distanceAfter > distanceBefor)
+        {
+            // Overshot
+            localTransform.ValueRW.Position = targetPosition;
+        }
+
+        float destroyDistanceSq = 0.2f;
+
+        if (math.distancesq(localTransform.ValueRO.Position, targetPosition) < destroyDistanceSq)
+        {
+            RefRW<Health> targethealth = healthComponentLookup.GetRefRW(target.targetEntity);
+            targethealth.ValueRW.healthAmount -= bullet.damageAmount;
+            targethealth.ValueRW.onHealthChanged = true;
+
+            ecb.DestroyEntity(entity);
         }
     }
 }
--- Assets/Scripts/Systems/FindTargetSystem.cs
+++ Assets/Scripts/Systems/FindTargetSystem.cs
@@ -3,22 +3,57 @@ using Unity.Entities;
 using Unity.Physics;
 using Unity.Transforms;
 using Unity.Collections;
-using UnityEngine;
 using Unity.Mathematics;
 
 partial struct FindTargetSystem : ISystem
 {
+    private ComponentLookup<LocalTransform> _localTransformComponentLookup;
+    private ComponentLookup<Unit> _unitComponentLookup;
+    private EntityStorageInfoLookup _entityStorageInfoLookup;
+
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        _localTransformComponentLookup = state.GetComponentLookup<LocalTransform>(true);
+        _unitComponentLookup = state.GetComponentLookup<Unit>(true);
+        _entityStorageInfoLookup = state.GetEntityStorageInfoLookup();
+    }
+
+    [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        PhysicsWorldSingleton physicsWorldSingleton = SystemAPI.GetSingleton<PhysicsWorldSingleton>();
-        CollisionWorld collisionWorld = physicsWorldSingleton.CollisionWorld;
-        NativeList<DistanceHit> distanceHitList = new NativeList<DistanceHit>(Allocator.Temp);
+        PhysicsWorldSingleton _physicsWorldSingleton = SystemAPI.GetSingleton<PhysicsWorldSingleton>();
+        CollisionWorld _collisionWorld = _physicsWorldSingleton.CollisionWorld;
+        NativeList<DistanceHit> _distanceHitList = new NativeList<DistanceHit>(Allocator.TempJob);
 
-        foreach ((RefRO<LocalTransform> localTransform, RefRW<FindTarget> findTarget, RefRW<Target> target, RefRO<TargetOverride> targetoverride) 
+        #region With Job
+
+        //_localTransformComponentLookup.Update(ref state);
+        //_unitComponentLookup.Update(ref state);
+        //_entityStorageInfoLookup.Update(ref state);
+
+        //FindTargetJob findTargetJob = new FindTargetJob
+        //{
+        //    deltaTime = SystemAPI.Time.DeltaTime,
+        //    physicsWorldSingleton = _physicsWorldSingleton,
+        //    collisionWorld = _collisionWorld,
+        //    distanceHitList = _distanceHitList,
+        //    localTransformComponentLookup = _localTransformComponentLookup,
+        //    unitComponentLookup = _unitComponentLookup,
+        //    entityStorageInfoLookup = _entityStorageInfoLookup
+        //};
+
+        //findTargetJob.ScheduleParallel();
+
+        #endregion
+
+        #region Without Job
+
+        foreach ((RefRO<LocalTransform> localTransform, RefRW<FindTarget> findTarget, RefRW<Target> target, RefRO<TargetOverride> targetoverride)
             in SystemAPI.Query<RefRO<LocalTransform>, RefRW<FindTarget>, RefRW<Target>, RefRO<TargetOverride>>())
         {
             findTarget.ValueRW.timer -= SystemAPI.Time.DeltaTime;
-            
+
             if (findTarget.ValueRW.timer > 0) continue;
             findTarget.ValueRW.timer = findTarget.ValueRO.timerMax;
 
@@ -28,7 +63,7 @@ partial struct FindTargetSystem : ISystem
                 continue;
             }
 
-            distanceHitList.Clear();
+            _distanceHitList.Clear();
             CollisionFilter collisionFilter = new CollisionFilter
             {
                 BelongsTo = ~0u,
@@ -37,7 +72,7 @@ partial struct FindTargetSystem : ISystem
             };
 
             Entity closestTargetEntity = Entity.Null;
-            float  closestTargetDistance = float.MaxValue;
+            float closestTargetDistance = float.MaxValue;
             float curTargetDistanceOffset = 0f;
 
             if (target.ValueRO.targetEntity != Entity.Null)
@@ -48,9 +83,9 @@ partial struct FindTargetSystem : ISystem
                 curTargetDistanceOffset = 2f;
             }
 
-            if (collisionWorld.OverlapSphere(localTransform.ValueRO.Position, findTarget.ValueRO.range, ref distanceHitList, collisionFilter))
+            if (_collisionWorld.OverlapSphere(localTransform.ValueRO.Position, findTarget.ValueRO.range, ref _distanceHitList, collisionFilter))
             {
-                foreach (DistanceHit distanceHit in distanceHitList)
+                foreach (DistanceHit distanceHit in _distanceHitList)
                 {
                     if (!SystemAPI.Exists(distanceHit.Entity) || !SystemAPI.HasComponent<Unit>(distanceHit.Entity)) continue;
 
@@ -71,15 +106,100 @@ partial struct FindTargetSystem : ISystem
                                 closestTargetDistance = distanceHit.Distance;
                             }
                         }
-                        
+
                     }
                 }
-            } 
+            }
 
             if (closestTargetEntity != Entity.Null)
             {
                 target.ValueRW.targetEntity = closestTargetEntity;
             }
+        }
+
+        #endregion
+
+    }
+}
+
+[BurstCompile]
+public partial struct FindTargetJob : IJobEntity
+{
+    public float deltaTime;
+
+    [ReadOnly] public PhysicsWorldSingleton physicsWorldSingleton;
+    [ReadOnly] public CollisionWorld collisionWorld;
+    [NativeDisableParallelForRestriction] public NativeList<DistanceHit> distanceHitList;
+
+    [ReadOnly] public ComponentLookup<LocalTransform> localTransformComponentLookup;
+    [ReadOnly] public ComponentLookup<Unit> unitComponentLookup;
+    [ReadOnly] public EntityStorageInfoLookup entityStorageInfoLookup;
+
+    public void Execute(ref FindTarget findTarget, ref Target target, in TargetOverride targetOverride, Entity entity)
+    {
+        findTarget.timer -= deltaTime;
+
+        if (findTarget.timer > 0) return;
+        findTarget.timer = findTarget.timerMax;
+
+        if (targetOverride.targetEntity != Entity.Null)
+        {
+            target.targetEntity = targetOverride.targetEntity;
+            return;
+        }
+
+        distanceHitList.Clear();
+        CollisionFilter collisionFilter = new CollisionFilter
+        {
+            BelongsTo = ~0u,
+            CollidesWith = 1u << GameAssets.UNIT_LAYER,
+            GroupIndex = 0
+        };
+
+        Entity closestTargetEntity = Entity.Null;
+        float closestTargetDistance = float.MaxValue;
+        float curTargetDistanceOffset = 0f;
+        LocalTransform localTransform = localTransformComponentLookup[entity];
+
+        if (target.targetEntity != Entity.Null)
+        {
+            closestTargetEntity = target.targetEntity;
+            LocalTransform targetLocalTransform = localTransformComponentLookup[closestTargetEntity];
+            closestTargetDistance = math.distance(localTransform.Position, targetLocalTransform.Position);
+            curTargetDistanceOffset = 2f;
+        }
+
+        if (collisionWorld.OverlapSphere(localTransform.Position, findTarget.range, ref distanceHitList, collisionFilter))
+        {
+            foreach (DistanceHit distanceHit in distanceHitList)
+            {
+                if (!entityStorageInfoLookup.Exists(distanceHit.Entity) || !unitComponentLookup.HasComponent(distanceHit.Entity)) continue;
+
+                Unit targetUnit = unitComponentLookup[distanceHit.Entity];
+                if (targetUnit.faction == findTarget.targetFaction)
+                {
+                    // Valid target
+                    if (closestTargetEntity == Entity.Null)
+                    {
+                        closestTargetEntity = distanceHit.Entity;
+                        closestTargetDistance = distanceHit.Distance;
+                    }
+                    else
+                    {
+                        if (distanceHit.Distance + curTargetDistanceOffset < closestTargetDistance)
+                        {
+                            closestTargetEntity = distanceHit.Entity;
+                            closestTargetDistance = distanceHit.Distance;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        if (closestTargetEntity != Entity.Null)
+        {
+            target.targetEntity = closestTargetEntity;
         }
     }
 }
